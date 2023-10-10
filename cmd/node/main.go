7e5ae0e9a54fa788e0ab93bcc4cdd15fc7f19707
Markku Rossi <mtr@iki.fi>
2023-10-10 08:12:18 +0100

    Stubs for client and node.

---
+++ cmd/node/main.go
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2023 Markku Rossi
+//
+// All rights reserved.
+//
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("SDCE Node")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/markkurossi/sdce
+
+go 1.21.0
